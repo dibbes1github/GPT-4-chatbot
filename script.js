3c27038cb8ae21fb8d211bb2008921b3f0d1eb82
dibbes1github <128802983+dibbes1github@users.noreply.github.com>
2023-03-25 14:23:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const chatInput = document.querySelector(".chat-input");
-    const chatSendBtn = document.querySelector(".chat-send-btn");
-    const chatMessages = document.querySelector(".chat-messages");
+  const chatInput = document.querySelector(".chat-input");
+  const chatMessages = document.querySelector(".chat-messages");
+  const chatForm = document.getElementById("chat-form");
 
-    // Handle sending message
-    chatSendBtn.addEventListener("click", function () {
-        sendMessage(chatInput.value);
-    });
+  // Handle sending message
+  chatForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    sendMessage(chatInput.value);
+  });
 
-    chatInput.addEventListener("keydown", function (event) {
-        if (event.key === "Enter") {
-            sendMessage(chatInput.value);
-        }
-    });
-
-    function sendMessage(message) {
-        if (!message.trim()) {
-            return;
-        }
-
-        // Display user message
-        const userMessage = document.createElement("div");
-        userMessage.classList.add("chat-message", "user-message");
-        userMessage.textContent = message;
-        chatMessages.appendChild(userMessage);
-        chatInput.value = "";
-
-        // Scroll to the bottom of the chat
-        chatMessages.scrollTop = chatMessages.scrollHeight;
-
-        // Send message to chatbot and display its response
-        getChatbotResponse(message).then((response) => {
-            const botMessage = document.createElement("div");
-            botMessage.classList.add("chat-message", "bot-message");
-            botMessage.textContent = response;
-            chatMessages.appendChild(botMessage);
-
-            // Scroll to the bottom of the chat
-            chatMessages.scrollTop = chatMessages.scrollHeight;
-        });
+  function sendMessage(message) {
+    if (!message.trim()) {
+      return;
     }
 
-    // Mock chatbot response
-    function getChatbotResponse(message) {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve("You said: " + message);
-            }, 1000);
-        });
+    // Display user message
+    const userMessage = document.createElement("div");
+    userMessage.classList.add("chat-message", "user-message");
+    userMessage.textContent = message;
+    chatMessages.appendChild(userMessage);
+    chatInput.value = "";
+
+    // Scroll to the bottom of the chat
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+    // Send message to chatbot and display its response
+    getChatbotResponse(message).then((response) => {
+      const botMessage = document.createElement("div");
+      botMessage.classList.add("chat-message", "bot-message");
+      botMessage.textContent = response;
+      chatMessages.appendChild(botMessage);
+
+      // Scroll to the bottom of the chat
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    });
+  }
+
+  // Update getChatbotResponse to call the API
+  async function getChatbotResponse(message) {
+    try {
+      const response = await fetch('/api/chatgpt', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      const data = await response.json();
+      return data.response;
+    } catch (error) {
+      console.error('Error fetching ChatGPT response:', error);
+      return 'An error occurred. Please try again.';
     }
+  }
 });
